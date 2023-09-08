1a1a0ebaa2e12fcd119cf0b2a991c68e53e61b8e
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-09-08 12:37:43 -0400

    .sh file to start local pa srvr & run docker cont.

--- start_audio_and_container.sh
+++ start_audio_and_container.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+#usage: ./start_audio_and_container.sh
+
 # Start the PulseAudio server
-./pulseaudio-1.1/bin/pulseaudio.exe --use-pid-file=false -D
+pulseaudio -D --exit-idle-time=-1
 
 # Give the server some time to initialize. Adjust as needed.
 sleep 5
 
-# Run the Docker container
+# Run the Docker container (remember this opens bash)
 docker run --name chatforme_gpt_t2s_container -e PULSE_SERVER=host.docker.internal -it chatforme_gpt_t2s_image
