864ed20332ecbf853f96cdd205bdfdafe98394dc
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-09-08 12:37:10 -0400

    untested for cmd.exe

--- voice_t2s_docker.bat
+++ voice_t2s_docker.bat
@@ -1,8 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 
+::##################
+::     UNTESTED
+::##################
+
 ::Set the image name
 set IMAGE_NAME=chatforme_gpt_t2s_image
+set CONTAINER_NAME=chatforme_gpt_t2s_container
+set DOCKERFILE_NAME=Dockerfile-alpine
 
 ::Check if Docker is installed
 docker --version > NUL 2>&1
@@ -11,13 +17,21 @@ if errorlevel 1 (
     exit /b
 )
 
+::Start the Pulse Audio Server
+echo Starting the pulse audio server. Note: starting daemon may faile
+pulseaudio -D --exit-idle-time=-1
+
 ::Build the Docker image
 echo Building Docker image...
-docker build -t %IMAGE_NAME% .
+docker build -f %DOCKERFILE_NAME% -t %IMAGE_NAME% .
 1
+
 ::Run the Docker container
 echo Running Docker container...
-docker run -it %IMAGE_NAME%
+docker run --name %CONTAINER_NAME% -e PULSE_SERVER=host.docker.internal -it %IMAGE_NAME%
+
+echo Starting python and running voice_t2s.py
+python3 voice_t2s.py
 
 ::End of script
 echo Done!
